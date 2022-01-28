1ec8c6cbcf35bb6ad013bcb01a50e654a4bb8cfa
IbraheemHaseeb7 <81369271+IbraheemHaseeb7@users.noreply.github.com>
2022-01-28 23:28:56 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { useData } from "../lib/hook";
 import { UserData } from "../components/Context";
 import { useEffect, useRef, useState } from "react";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const { hello } = useData();
@@ -28,6 +29,25 @@ function MyApp({ Component, pageProps }) {
         src="../video/video.mp4"
       />
       <Component {...pageProps} />;
+      <Head>
+        <title>Send Secret Messages</title>
+        <meta
+          name="description"
+          content="Create an account using twitter or google and receive annonymous messages from people with whom you share the link"
+        />
+        <meta
+          name="keywords"
+          content="Secret Messages, Secrets, Viralsachxd, viralsachxd"
+        />
+        <meta name="author" content="Ibraheem Bin Haseeb" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Send Secret Messages" />
+        <meta property="og:url" content="https://secretmsgme.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://t3.ftcdn.net/jpg/01/43/44/90/360_F_143449039_BIJaOitahmKDzQbX969cCESmVfXSzeN1.jpg"
+        />
+      </Head>
       <Toaster />
       {mute ? (
         <button className="mute" onClick={muter}>
